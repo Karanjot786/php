0c800da2a2670f462299c0dcd95aae41abe85369
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-08-20 09:04:15 +0530

    Update 13_p.php

--- 13_p.php
+++ 13_p.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Palindrome Checker</title>
+</head>
 <body>
+    <h1>Palindrome Checker</h1>
+    <form method="post" action="">
+        <label for="input">Enter a string:</label>
+        <input type="text" name="input" required><br>
 
-<?php
-echo "My first PHP script!";
-?>
+        <input type="submit" name="check" value="Check">
+    </form>
 
+    <?php
+    if(isset($_POST['check'])) {
+        $input = strtolower($_POST['input']); // Convert to lowercase for case-insensitive check
+        $input = preg_replace("/[^a-zA-Z0-9]/", "", $input); // Remove non-alphanumeric characters
+
+        $reversed = strrev($input); // Reverse the string
+
+        if ($input === $reversed) {
+            echo "<p>The string \"$input\" is a palindrome.</p>";
+        } else {
+            echo "<p>The string \"$input\" is not a palindrome.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
+
