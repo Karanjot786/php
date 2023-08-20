a52020802d42b1975ff12349ff4179a979ba04b0
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-08-20 09:05:10 +0530

    Update 16_p.php

--- 16_p.php
+++ 16_p.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Letter Case Counter</title>
+</head>
 <body>
+    <h1>Letter Case Counter</h1>
+    <form method="post" action="">
+        <label for="input">Enter a string:</label>
+        <input type="text" name="input" required><br>
 
-<?php
-echo "My first PHP script!";
-?>
+        <input type="submit" name="count" value="Count">
+    </form>
 
+    <?php
+    if(isset($_POST['count'])) {
+        $input = $_POST['input'];
+
+        $uppercaseCount = 0;
+        $lowercaseCount = 0;
+
+        for ($i = 0; $i < strlen($input); $i++) {
+            if (ctype_upper($input[$i])) {
+                $uppercaseCount++;
+            } elseif (ctype_lower($input[$i])) {
+                $lowercaseCount++;
+            }
+        }
+
+        echo "<p>Input string: $input</p>";
+        echo "<p>Uppercase letters count: $uppercaseCount</p>";
+        echo "<p>Lowercase letters count: $lowercaseCount</p>";
+    }
+    ?>
 </body>
 </html>
