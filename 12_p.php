4f738756fcbe13db8e6cdce240f7adecbfcba377
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-08-20 09:03:59 +0530

    Update 12_p.php

--- 12_p.php
+++ 12_p.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Vowel Counter</title>
+</head>
 <body>
+    <h1>Vowel Counter</h1>
+    <p>Phrase: "Develop & Empower Individuals"</p>
 
-<?php
-echo "My first PHP script!";
-?>
+    <?php
+    if(isset($_POST['count'])) {
+        $phrase = "Develop & Empower Individuals";
+        $vowels = array('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
 
+        $count = 0;
+        for ($i = 0; $i < strlen($phrase); $i++) {
+            if (in_array($phrase[$i], $vowels)) {
+                $count++;
+            }
+        }
+
+        echo "<p>Total number of vowels: $count</p>";
+    }
+    ?>
+
+    <form method="post" action="">
+        <input type="submit" name="count" value="Count Vowels">
+    </form>
 </body>
 </html>
+
