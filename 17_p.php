11a1135ace7c5ec1056e588f5d14451d12838b7c
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-08-20 09:05:29 +0530

    Update 17_p.php

--- 17_p.php
+++ 17_p.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>String Reversal</title>
+</head>
 <body>
+    <h1>String Reversal</h1>
+    <form method="post" action="">
+        <label for="input">Enter a string:</label>
+        <input type="text" name="input" required><br>
 
-<?php
-echo "My first PHP script!";
-?>
+        <input type="submit" name="reverse" value="Reverse">
+    </form>
 
+    <?php
+    if(isset($_POST['reverse'])) {
+        $input = $_POST['input'];
+        $words = explode(" ", $input); // Split the input string into words
+        $reversedWords = array_reverse($words); // Reverse the array of words
+        $reversedString = implode(" ", $reversedWords); // Join the reversed words back into a string
+
+        echo "<p>Original string: $input</p>";
+        echo "<p>Reversed string: $reversedString</p>";
+    }
+    ?>
 </body>
 </html>
