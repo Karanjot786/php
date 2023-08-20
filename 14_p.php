f8f5fc2453a8b736ca59a0274e0123def0c3fb0e
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-08-20 09:04:33 +0530

    Update 14_p.php

--- 14_p.php
+++ 14_p.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Alphabetical Sorting</title>
+</head>
 <body>
+    <h1>Alphabetical Sorting</h1>
+    <form method="post" action="">
+        <label for="input">Enter a word:</label>
+        <input type="text" name="input" required><br>
 
-<?php
-echo "My first PHP script!";
-?>
+        <input type="submit" name="sort" value="Sort">
+    </form>
 
+    <?php
+    if(isset($_POST['sort'])) {
+        $input = $_POST['input'];
+        $sorted = str_split($input); // Split the string into an array of characters
+        sort($sorted); // Sort the array in alphabetical order
+        $sortedString = implode("", $sorted); // Convert the sorted array back to a string
+
+        echo "<p>Original word: $input</p>";
+        echo "<p>Word after sorting: $sortedString</p>";
+    }
+    ?>
 </body>
 </html>
